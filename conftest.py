1e34f821b31d37122333518c7bcae48648e2a812
inginiar <inginiar@gmail.com>
2024-03-11 15:58:48 +0300

    импортируется ChromiumOptions вместо Options

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import ChromiumOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 
 def pytest_addoption(parser):
@@ -16,13 +16,13 @@ def browser(request):
     user_language = request.config.getoption("language")
 
     if browser_name == "chrome":
-        options = Options()
+        options = ChromiumOptions()
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         print("\nstart chrome browser for test..")
         browser = webdriver.Chrome(options=options)
 
     elif browser_name == "firefox":
-        options = Options()
+        options = FirefoxOptions()
         options.set_preference("intl.accept_languages", user_language)
         browser = webdriver.Firefox(options=options)
 
--- test_items.py
+++ test_items.py
@@ -6,6 +6,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_includes_a_add_to_cart_button(browser):
     browser.get(link)
     #time.sleep(30)
-    assert browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+    button_basket = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+    assert button_basket != [], "'Add to cart' button not found."
 
     
